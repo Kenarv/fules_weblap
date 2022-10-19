5948d763c46aaf2a8a5f294020f77fb02ab8b76d
Kenarv <balintvranek@gmail.com>
2022-10-19 18:38:52 +0200

    Version_1

---
+++ slidescript.js
@@ -0,0 +1,22 @@
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) => {
+    panel.addEventListener("click", () => {
+        removeActiveClasses();
+        panel.classList.add("active");
+        panel.classList.remove("blur");
+    });
+});
+
+const removeActiveClasses = () => {
+    panels.forEach((panel) => {
+        panel.classList.remove("active");
+        panel.classList.add("blur");
+    });
+};
+
+
+
+
+
+
